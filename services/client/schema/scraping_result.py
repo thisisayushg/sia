e181b1f254d05b056b648cc649d0efe1359dd6c9
Ayush Gupta <thisisayush@protonmail.com>
2026-01-28 00:48:00 +0530

    Added scraping result schema: Fixed runtime error

---
+++ services/client/schema/scraping_result.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Field
+from typing import Generic, TypeVar
+
+T = TypeVar("T")
+
+class ScrapingResult(BaseModel):
+    url: str = Field(description="URL/Link to the website")
+    domain: str = Field(description="Domain of the website")
+    title: str = Field(description="Title/Heading of the page")
+    search_term: str = Field(description="Search term/ text which was searched on the web")
+    content: str = Field(description="Content found after scraping the webpage")
+
+class ScrapingResultCollection(BaseModel):
+    scraping_results: list[ScrapingResult] = Field("A collection of scraping result of each url")
