4a282a903cdbf38e2466476390ef0c0d9e2f1ccf
Ayush Gupta <thisisayush@protonmail.com>
2026-02-25 01:10:27 +0530

    Fixed graph state to use with NER model

--- services/client/schema/graph_states.py
+++ services/client/schema/graph_states.py
@@ -16,7 +16,7 @@ class ElicitationState(SupervisorState):
 class RecommendationState(ElicitationState):
     requirements_gathered: Dict = {}
     scraping_results: Annotated[list[ScrapingResult], operator.add] = None
-    extracted_names: Annotated[list[NameExtractionResult], operator.add] = None
+    extracted_names: Annotated[list[str], operator.add] = None
     web_search_results: list = []  # Type Annotation for each field in a GraphState is very important else it will not be injected in the state for next node, even if previous node returns a certain key
     recommendation: str = ''
     investigation_report: Annotated[str, operator.concat] = ''
