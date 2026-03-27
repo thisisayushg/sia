765e8107716bc03a131cc0de87c82c23baa7b107
Ayush Gupta <thisisayush@protonmail.com>
2026-03-27 21:22:12 +0530

    Fixed default values for destination recommendation (needed for SLM)

--- services/client/schema/destination_recommendation.py
+++ services/client/schema/destination_recommendation.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime, timedelta
 from pydantic import BaseModel, Field
 from shared.utils.pydantic_validator import PartialValidationMixin
 from typing import Annotated, Optional, List
@@ -10,8 +10,8 @@ class DestinationRecommendation(PartialValidationMixin, BaseModel):
     purpose: Annotated[Optional[str], Field(None, description="Purpose of travel such as relaxation, adventure, culture, family time,")]
     duration: Annotated[Optional[int], Field(1, description="Duration of the travel including the transportation and stay (in number of days)")]
     season: Annotated[Optional[str], Field('', description="Season if specified by the user")]
-    trip_start: Annotated[date, Field(..., description="Expected start date of the trip. Infer using date interpretation rules, if not provided")]
-    trip_end: Annotated[date, Field(..., description="Expected end date of the trip. Infer using date interpretation rules, if not provided")]
+    trip_start: Annotated[date, Field(datetime.now().date(), description="Expected start date of the trip. Infer using date interpretation rules, if not provided")]
+    trip_end: Annotated[date, Field((datetime.now() + timedelta(days=1)).date(), description="Expected end date of the trip. Infer using date interpretation rules, if not provided")]
     start_location: Annotated[Optional[str], Field('', description="Start location of the trip. Could be current location of the user as well.")]
     region: Annotated[Optional[str], Field('', description="A region like country, a continent, or a sub-region like North-East India specified by the user to recommend destinations within.")]
     reasoning: Annotated[str, Field('', description="All the reasoning behind each of the extracted value. Also include current date in words in this reasoning.  This is to be populated by you and not user")]
