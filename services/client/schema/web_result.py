8a991941dcd904925e3fcf2ffd1c9aff4a28d378
Ayush Gupta <thisisayush@protonmail.com>
2026-04-05 03:38:15 +0530

    Made few fields optional in web result

--- services/client/schema/web_result.py
+++ services/client/schema/web_result.py
@@ -1,11 +1,11 @@
 from pydantic import BaseModel, Field
-
+from typing import Optional
 
 class WebSearchResult(BaseModel):
     url: str = Field(description="URL/Link to the website")
-    domain: str = Field(description="Domain of the website")
+    domain: Optional[str] = Field(description="Domain of the website")
     title: str = Field(description="Title/Heading of the page")
-    search_term: str = Field(description="Search term/ text which was searched on the web")
+    search_term: Optional[str] = Field(description="Search term/ text which was searched on the web")
 
 class WebSearchResultCollection(BaseModel):
     search_results: list[WebSearchResult] = Field(description="A Collection of all web search results")
